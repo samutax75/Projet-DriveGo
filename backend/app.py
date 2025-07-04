8445d764db98521713bf8afd150316e0074906b5
Sam <samirbenhammou94250@gmail.com>
2025-07-04 16:13:02 +0200

    Mise à jour des @app.route flask

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,19 @@
-from flask import Flask,render_template,redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/inscription')
+def inscription():
+    return render_template('inscription.html')
 
 
+@app.route('/connexion')
+def connexion():
+    return render_template('connexion.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
