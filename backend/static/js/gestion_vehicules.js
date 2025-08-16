348f502e65477fdb9310f24a7aabe9ccf937f30c
Sam <samirbenhammou94250@gmail.com>
2025-08-16 15:58:04 +0100

    Mise à jour départ mission

--- backend/static/js/gestion_vehicules.js
+++ backend/static/js/gestion_vehicules.js
@@ -321,12 +321,7 @@
                                            value="${new Date().toTimeString().slice(0, 5)}" required>
                                 </div>
 
-                                <div class="form-row">
-                                <div class="form-group">
-                                    <label for="departureTime">🕐 Heure de retour</label>
-                                    <input type="time" id="departureTime" name="departureTime" 
-                                           value="${new Date().toTimeString().slice(0, 5)}" required>
-                                </div>
+                                
                                 
                                 <div class="form-group">
                                     <label for="passengers">👥 Nombre de passagers</label>
